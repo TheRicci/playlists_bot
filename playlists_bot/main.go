720e0e1881fbb52a9fd8316c6c5c945da7a33e4d
TheRicci <41972780+TheRicci@users.noreply.github.com>
2024-03-26 20:02:12 -0300

    new youtube file

--- playlists_bot/main.go
+++ playlists_bot/main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/rs/zerolog/log"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
-	"google.golang.org/api/option"
-	youtube "google.golang.org/api/youtube/v3"
 )
 
 type Bot struct {
@@ -21,6 +17,14 @@ type Bot struct {
 	*database
 	randomMap         map[string]*[]videoQuery //no need to use mutex, every key will only be accessed by one user from one command
 	openCommandRandom map[string]*discordgo.Message
+	openCommandSearch map[string]MenuSelectionState
+}
+
+type MenuSelectionState struct {
+	currentIndex int
+	maxIndex     int
+	videos       []videoQuery
+	list         []*[]discordgo.SelectMenuOption
 }
 
 func newBot() Bot {
@@ -34,66 +38,8 @@ func newBot() Bot {
 		newDB(),
 		make(map[string]*[]videoQuery),
 		make(map[string]*discordgo.Message),
+		make(map[string]MenuSelectionState),
 	}
-}
-
-func playlistVideos(service *youtube.Service, part, playlistId, token string) (*youtube.PlaylistItemListResponse, error) {
-	call := service.PlaylistItems.List([]string{part}).PlaylistId(playlistId).MaxResults(50)
-
-	if token != "" {
-		call = call.PageToken(token)
-	}
-
-	return call.Do()
-}
-
-func fetchVideos(playlistID string) (*[]Video, error) {
-	ctx := context.Background()
-	youtubeService, err := youtube.NewService(ctx, option.WithAPIKey(os.Getenv("YOUTUBE_KEY")))
-	if err != nil {
-		log.Fatal().Msgf("Error creating YouTube client: %v", err)
-	}
-
-	var videos []Video
-	pageToken := ""
-	now := time.Now()
-	m := make(map[string]struct{})
-	for {
-		response, err := playlistVideos(youtubeService, "snippet", playlistID, pageToken)
-		if err != nil {
-			return nil, err
-		}
-
-		for _, item := range response.Items {
-			if item.Snippet.Title == "Private video" || item.Snippet.Title == "Deleted video" {
-				continue
-			}
-			if _, ok := m[item.Snippet.ResourceId.VideoId]; ok {
-				continue
-			} else {
-				m[item.Snippet.ResourceId.VideoId] = struct{}{}
-			}
-
-			fmt.Println(item.Snippet.ResourceId.VideoId, ": ", item.Snippet.Title)
-			videos = append(videos, Video{
-				ID:            item.Snippet.ResourceId.VideoId,
-				Title:         item.Snippet.Title,
-				Thumbnail:     item.Snippet.Thumbnails.Medium.Url,
-				Channel_id:    item.Snippet.ChannelId,
-				Channel_title: item.Snippet.ChannelTitle,
-				Updated_at:    &now,
-				Created_at:    &now,
-			})
-		}
-
-		if response.NextPageToken == "" {
-			break
-		}
-
-		pageToken = response.NextPageToken
-	}
-
-	return &videos, nil
 }
 
 func main() {
